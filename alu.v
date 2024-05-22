e7ec498f4024eed8e01829e15ae479b806a421e7
Nada Omar <109097592+na0da2021@users.noreply.github.com>
2024-05-22 14:09:24 +0300

    Update alu.v

--- alu.v
+++ alu.v
@@ -71,12 +71,4 @@ end
 
 end			  
 
-endmodule							
-
-// 1_bit full adder
-module f_a_s( output sum, cout,
-              input a, b, cin
-             ); 			 				 
-assign sum = a ^ b ^ cin;
-assign cout = 	(a & b) | (cin & (a ^ b));
 endmodule
